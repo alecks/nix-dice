bb202d55ee3ef4feab3ea6f87ffc265becbeedad
Alex Pedley <alex@pedley.org>
2025-02-02 15:26:16 +0000

    clean up curl downloads and information after installation

--- install.sh
+++ install.sh
@@ -8,16 +8,30 @@ cleanup() {
 
 fail() {
   echo "FAIL: $1"
-  cleanup
+  echo "You may need to remove ~/.nix and check ~/.zshrc if the installation is incomplete."
   exit 1
+}
+
+hardfail() {
+  cleanup
+  fail "$1"
 }
 
 echo "This installer will add the with-nix and enter-nix scripts to your ~/.local/bin directory. It will then proceed to run the Nix installer, adding the relevant configurations to your ~/.zshrc."
 echo ""
 
-mkdir -p ~/.local/bin || fail "Couldn't create ~/.local/bin directory"
-curl -L https://github.com/nix-community/nix-user-chroot/releases/download/1.2.2/nix-user-chroot-bin-1.2.2-x86_64-unknown-linux-musl -o ~/.local/bin/nix-user-chroot
-# TODO: enter-nix and with-nix
+mkdir -p ~/.local/bin || hardfail "Couldn't create ~/.local/bin directory."
+declare -A files=(
+    ["https://github.com/nix-community/nix-user-chroot/releases/download/1.2.2/nix-user-chroot-bin-1.2.2-x86_64-unknown-linux-musl"]="$HOME/.local/bin/nix-user-chroot"
+    ["https://github.com/alecks/nix-dice/raw/refs/heads/main/with-nix.sh"]="$HOME/.local/bin/with-nix"
+    ["https://github.com/alecks/nix-dice/raw/refs/heads/main/enter-nix.sh"]="$HOME/.local/bin/enter-nix"
+)
+
+for url in "${!files[@]}"; do
+    dest="${files[$url]}"
+    curl -L "$url" -o "$dest" || fail "Download failed for $url"
+    chmod +x "$dest"
+done
 
 ZSHRC="$HOME/.zshrc"
 RC_NIX_SETUP='
@@ -25,7 +39,7 @@ RC_NIX_SETUP='
 export PATH="$HOME/.local/bin:$PATH"
 # If we haven'\''t already asked to enter nix, ask.
 if [[ -z "$DONE_NIX_CHECK" ]]; then
-  exec enter-nix zsh "$@"
+  exec enter-nix zsh
 fi
 # END NIX FOR DICE
 
@@ -40,20 +54,28 @@ else
   echo "Nix setup already present in ~/.zshrc, skipping..."
 fi
 
-mkdir -m 0755 ~/.nix || fail "Couldn't to create ~/.nix directory"
+mkdir -p -m 0755 ~/.nix || hardfail "Couldn't to create ~/.nix directory"
 
 echo "START OF NIX INSTALL SCRIPT"
 ~/.local/bin/nix-user-chroot ~/.nix bash -c "curl -L https://nixos.org/nix/install | bash" || fail "Nix install script failed."
 
-echo ""
-echo "DONE: Please restart your shell and enter zsh. You will be prompted to answer y/n to enter the Nix environment."
-echo "You will then be able to use nix-env -i <package> to install packages. Refer to the Nix documentation for more info."
-echo ""
-echo "To enter the Nix environment every time without prompting, add the following to your .zshrc:"
-echo "export BYPASS_NIX_CHECK=1"
-echo ""
-echo "To always enter the normal environment by default add the following:"
-"export BYPASS_NIX_CHECK_NORMAL=1"
-echo ""
-echo "Then you can manually open a Nix shell by running with-nix zsh."
-echo "Otherwise, you will be prompted every time."
+cat <<EOF
+
+DONE: Nix for DICE has been installed. Please restart your shell and enter zsh. You will be prompted to answer y/n to enter the Nix environment.
+
+You will then be able to use:
+  nix-env -i <package>   # to install packages
+  nix-shell -p <package> # to try out a package in a temporary environment
+Refer to the Nix documentation for more info.
+
+To enter the Nix environment every time without prompting, add this to the top of your .zshrc:
+  export BYPASS_NIX_CHECK=1
+
+To always enter the normal environment by default, add:
+  export BYPASS_NIX_CHECK_NORMAL=1
+
+Then, you can manually open a Nix shell by running:
+  with-nix zsh
+
+EOF
+
