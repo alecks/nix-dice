6e6553135de3bdc6bacba9de315a481adcccce66
Alex Pedley <alex@pedley.org>
2025-02-04 22:58:03 +0000

    don't error if nix-user-chroot isn't found

--- with-nix.sh
+++ with-nix.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-nix-user-chroot ~/.nix "$@"
+
+if command -v nix-user-chroot 2>/dev/null; then
+    exec nix-user-chroot ~/.nix "$@"
+else
+    echo "nix-user-chroot could not be found, continuing without Nix"
+    exec "$@"
+fi
